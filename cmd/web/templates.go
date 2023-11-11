213e551ec4f53a6b436475eae12bc0f2799f115f
Lucas Farias <lucasf1@gmail.com>
2023-11-11 06:01:23 -0300

    Uso de custom function nos templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,17 +3,27 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/lucasf1/snippetbox/internal/models"
 )
 
 type templateData struct {
-	Snippet  models.Snippet
-	Snippets []models.Snippet
+	Snippet     models.Snippet
+	Snippets    []models.Snippet
 	CurrentYear int
 }
 
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
+
 	cache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./ui/html/pages/*.gohtml")
@@ -22,9 +32,10 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	}
 
 	for _, page := range pages {
+
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.gohtml")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.gohtml")
 		if err != nil {
 			return nil, err
 		}
